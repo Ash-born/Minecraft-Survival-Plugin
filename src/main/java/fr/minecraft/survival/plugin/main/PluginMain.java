6dafafa96acf91a4479a05f2c224a760978d8763
MarronEyes <marounyeux@gmail.com>
2021-07-17 16:12:31 +0100

    Added auction support to plugin.

--- src/main/java/fr/minecraft/survival/plugin/main/PluginMain.java
+++ src/main/java/fr/minecraft/survival/plugin/main/PluginMain.java
@@ -1,15 +1,13 @@
 package fr.minecraft.survival.plugin.main;
 
-import fr.minecraft.survival.plugin.commands.DelHome;
-import fr.minecraft.survival.plugin.commands.Home;
-import fr.minecraft.survival.plugin.commands.Points;
-import fr.minecraft.survival.plugin.commands.SetHome;
+import fr.minecraft.survival.plugin.commands.*;
 import fr.minecraft.survival.plugin.events.onConnexion;
 import fr.minecraft.survival.plugin.utils.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginMain extends JavaPlugin  {
-    public  static PluginMain instance;
+    public static PluginMain instance;
+    public static XML xml = new XML();
 
     public static PluginMain getInstance() {
         return instance;
@@ -17,15 +15,19 @@ public class PluginMain extends JavaPlugin  {
 
     @Override
     public void onEnable() {
+        XML.create_xml(null,null);
 
         getLogger().info("onEnable has been invoked!");
         getServer().getPluginManager().registerEvents(new onConnexion(), this);
-        XML.create_xml(null,null);
+
         getCommand("points").setExecutor(new Points());
         getCommand("sethome").setExecutor(new SetHome());
         getCommand("home").setExecutor(new Home());
         getCommand("delhome").setExecutor(new DelHome());
+        getCommand("bet").setExecutor(new Bet());
+
         instance = this;
+        new Bid();
 
     }
 
